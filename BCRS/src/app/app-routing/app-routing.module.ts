5398f10531eeedf8b52e5951de3d554967566a45
William Simpson <simpswc3911@gmail.com>
2018-10-24 20:45:10 -0500

    adding routing functionality to sign-in,user-management,security-questions page

--- BCRS/src/app/app-routing/app-routing.module.ts
+++ BCRS/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInPageComponent } from '../sign-in-page/sign-in-page.component';
+import { SecurityQuestionPageComponent } from '../security-question-page/security-question-page.component';
+import { UserManagementPageComponent } from '../user-management-page/user-management-page.component';
+
+
+
+const appRoutes: Routes = [
+
+{ path: '', component: SignInPageComponent },
+{ path: 'sign-in', component: SignInPageComponent },
+{ path: 'security-questions-create', component: SecurityQuestionPageComponent },
+{ path: 'user-management', component: UserManagementPageComponent }
+
+
+];
+
+
+
+
+
+
+
+
+
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(appRoutes, { useHash: true, enableTracing : true}) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- BCRS/src/app/app.module.ts
+++ BCRS/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+// import {FormControl, Validators} from '@angular/forms';
+import {MatCardModule, MatCard} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule }    from '@angular/common/http';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { UserManagementPageComponent } from './user-management-page/user-management-page.component';
 import { SecurityQuestionPageComponent } from './security-question-page/security-question-page.component';
+import { Browser } from 'protractor';
 
 @NgModule({
   declarations: [
@@ -14,7 +27,18 @@ import { SecurityQuestionPageComponent } from './security-question-page/security
     SecurityQuestionPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    // FormControl,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatRadioModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
