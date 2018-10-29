652c50351d6a4f2a0167a2788c4b13380d904e7a
Matthew Howard <mhoward14@icloud.com>
2018-10-29 17:50:56 -0600

    Adding BCRS Files

--- BCRS/src/app/app.module.ts
+++ BCRS/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppComponent } from './app.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { UserManagementPageComponent } from './user-management-page/user-management-page.component';
 import { SecurityQuestionPageComponent } from './security-question-page/security-question-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '../../node_modules/@angular/forms';
+import { MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule, MatCardModule, } from '@angular/material';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { LayoutModule, } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,22 @@ import { SecurityQuestionPageComponent } from './security-question-page/security
     SecurityQuestionPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatListModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
